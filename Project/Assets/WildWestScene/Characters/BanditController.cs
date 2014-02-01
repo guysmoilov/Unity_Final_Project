aceac41e6be6af106be5c8462b90f0502f29980d
Guy Smoilov <guy.smoilov@gmail.com>
2014-02-01 21:35:12 +0200

    Created bandit state transitions

--- Project/Assets/WildWestScene/Characters/BanditController.cs
+++ Project/Assets/WildWestScene/Characters/BanditController.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class BanditController : MonoBehaviour
 {
 	public float MinDistanceForChase = 20.0f;
+	public float sheriffFleeDistance = 30f;
+
 	private const float MIN_TIME_TO_RECALC_PATH = 0.2f;
 	private StackFSM brain;
 	private PathSeeker seeker;
 	private Animator animator;
 	private TextMesh textMesh;
 	private GameObject MinerToChase;
+	private GameObject sheriff;
 	private float deltaTimeToRecalcPath;
 
 
@@ -21,10 +24,10 @@ public class BanditController : MonoBehaviour
 		textMesh = GetComponentInChildren<TextMesh>();
 
 		MinerToChase = GameObject.Find ("Miner");
+		sheriff = GameObject.Find("Sheriff");
 		RegenrateNewTarget ();
 		textMesh.text = "Bandit: Roaming";
-		brain.PushState (Wander);
-		animator.Play ("Moving");
+		brain.PushState (StartWander);
 
 		deltaTimeToRecalcPath = 0;
 
@@ -36,21 +39,40 @@ public class BanditController : MonoBehaviour
 		seeker.SetTarget (new Vector3(Random.Range (-120, 120), Random.Range (-120, 120), Random.Range (-120, 120)));
 	}
 
+	void StartWander()
+	{
+		Debug.Log("Bandit: starts wandering");
+		animator.Play("Moving");
+
+		brain.PopState();
+		brain.PushState(Wander);
+	}
+
 	void Wander()
 	{
 		if (seeker.SeekPath ())// if there is no path, or we reached the end of the path
 		{ 
-						RegenrateNewTarget ();
+			RegenrateNewTarget ();
 		}
 		else 
 		{
+			// If sheriff is close, run away
+			if(Vector3.Distance(sheriff.transform.position,transform.position) < sheriffFleeDistance)
+			{
+				animator.Play("Running");
+				brain.PopState();
+				brain.PushState(Wander);
+				brain.PushState(Flee);
+			}
 			// If miner is close and alive, chase him
-			if(Vector3.Distance(MinerToChase.transform.position,transform.position) < MinDistanceForChase &&
+			else if(Vector3.Distance(MinerToChase.transform.position,transform.position) < MinDistanceForChase &&
 			   MinerToChase.GetComponent<StackFSM>().PeekState() != MinerToChase.GetComponent<MinerController>().Dead)
 			{
 				seeker.SetTarget(MinerToChase.transform.position);
 				animator.Play("Running");
 				textMesh.text = "Bandit: Chasing Miner";
+				brain.PopState();
+				brain.PushState(StartWander);
 				brain.PushState(Chase);
 			}
 		}
@@ -58,8 +80,13 @@ public class BanditController : MonoBehaviour
 
 	void Chase()
 	{
+		// If sheriff is close, run away
+		if(Vector3.Distance(sheriff.transform.position,transform.position) < sheriffFleeDistance)
+		{
+			brain.PushState(Flee);
+		}
 		// If miner is far or dead, abandon the chase
-		if (Vector3.Distance (MinerToChase.transform.position, transform.position) > MinDistanceForChase + 2.0f ||
+		else if (Vector3.Distance (MinerToChase.transform.position, transform.position) > MinDistanceForChase + 2.0f ||
 		    MinerToChase.GetComponent<StackFSM>().PeekState() == MinerToChase.GetComponent<MinerController>().Dead)
 		{
 						brain.PopState ();
@@ -85,5 +112,12 @@ public class BanditController : MonoBehaviour
 
 	void Flee()
 	{
+		textMesh.text = "Bandit: Running away from the fuzz!";
+
+		// If sheriff is far away, stop running
+		if (Vector3.Distance(sheriff.transform.position,transform.position) > sheriffFleeDistance)
+		{
+			brain.PopState();
+		}
 	}
 }
