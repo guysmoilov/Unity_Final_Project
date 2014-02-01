5d7492e281d3987e259ac2be42351b1f3550f9cf
Guy Smoilov <guy.smoilov@gmail.com>
2014-02-02 00:36:20 +0200

    Mouse shoot script changes bird's color

---
+++ Project/Assets/ThirdScene/MouseShootScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseShootScript : MonoBehaviour 
+{
+	public Material redMaterial;
+	public Material blueMaterial;
+	public Material greenMaterial;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+
+			Debug.DrawRay(Camera.main.ScreenPointToRay(Input.mousePosition).origin,
+			              Camera.main.ScreenPointToRay(Input.mousePosition).direction * 100,
+			              Color.red);
+
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+			{
+				if (hit.transform.parent.tag == "LeaderBird")
+				{
+					Debug.Log("WIN " + hit.transform.parent.name);
+
+					var renderers = hit.transform.parent.GetComponentsInChildren<MeshRenderer>();
+
+					foreach (var renderer in renderers)
+					{
+						renderer.material = greenMaterial;
+					}
+				}
+				else
+				{
+					Debug.Log(hit.transform.parent.name);
+
+					var renderers = hit.transform.parent.GetComponentsInChildren<MeshRenderer>();
+					
+					foreach (var renderer in renderers)
+					{
+						renderer.material = redMaterial;
+					}
+
+					renderers = GameObject.FindGameObjectWithTag("LeaderBird").GetComponentsInChildren<MeshRenderer>();
+
+					foreach (var renderer in renderers)
+					{
+						renderer.material = blueMaterial;
+					}
+				}
+			}
+		}
+	}
+}
